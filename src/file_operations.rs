7dcad7299d55bbed16620a4773422a5a7506dfc6
Ferenczi Janos <janoferenczi@yahoo.com>
2024-12-02 16:58:06 +0200

    Implemented read file into vector of &str line by line

--- src/file_operations.rs
+++ src/file_operations.rs
@@ -1,3 +1,11 @@
-pub fn read_file(_path: &str) {
+use std::fs;
+
+pub fn read_file(_path: &str) -> std::io::Result<()> {
     println!("Reading file from path {_path}");
+    let file_content = fs::read_to_string(_path)?;
+    let lines:Vec<&str> = file_content.split("\n").collect();
+
+    println!("The linse are {:?}", lines);
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
 use std::env;
-use std::error::Error;
 
 mod file_operations;
 mod help;
 
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> std::io::Result<()> {
 
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
         println!("No input was providede!");
-        help::print_usage();        
+        help::print_usage();
 
         return Ok(());
     }
 
     let path: &str = args[1].as_str();
-    file_operations::read_file(path); 
-    
+    file_operations::read_file(path);
+
     Ok(())
 }
